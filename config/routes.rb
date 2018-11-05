fec0a7ac8ff9f5a6301e694e48e81a94ee634597
Jane <jane.edwards.mcnear@gmail.com>
2018-11-05 13:49:23 -0800

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  # post "/movies", to: "movies#create", as: "create_movie"
-  # post "/rentals/check-out", to: "rentals#check_out", as: "check_out_movie"
-  # post "/rentals/check-in, to: "rentals#check_in", as: "check_in_movie"
+  get "/movies", to: "movies#index", as: "movies"
+  post "/movies", to: "movies#create", as: "create_movie"
+  get "/movies/:id", to: "movies#show", as: "movie"
 
+  post "/rentals/check-out", to: "rentals#check_out", as: "check_out_movie"
+  post "/rentals/check-in, to: "rentals#check_in", as: "check_in_movie"
+
+  get "/customers", to: "customers#index", as: "customers"
 
 end
