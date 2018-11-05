e9d4a1f6253b8a777d3ed48a23560ededb2305dd
Jane <jane.edwards.mcnear@gmail.com>
2018-11-05 15:23:51 -0800

    added customer model tests

--- test/models/customer_test.rb
+++ test/models/customer_test.rb
@@ -1,9 +1,61 @@
 require "test_helper"
 
 describe Customer do
-  let(:customer) { Customer.new }
 
-  it "must be valid" do
-    value(customer).must_be :valid?
+  describe "relationships" do
+
+    it "has many rentals" do
+      customer = customers(:mike)
+
+      customer.must_respond_to :rentals
+
+      customer.rentals.each do |rental|
+        rental.customer.must_be_kind_of Customer
+      end
+    end
+
   end
+
+  describe "validations" do
+    it "must have a name" do
+      customer = customers(:mike)
+
+      customer.name = nil
+      result = customer.save
+      result.must_equal false
+
+      customer.name = "Jane"
+      result = customer.save
+      result.must_equal true
+    end
+
+    it "must have movies checked out count" do
+      customer = customers(:mike)
+
+      customer.movies_checked_out_count = nil
+      result = customer.save
+      result.must_equal false
+
+      customer.movies_checked_out_count = 1
+      result = customer.save
+      result.must_equal true
+    end
+
+    it "must have 0 or more movies checked out" do
+      customer = customers(:mike)
+
+      customer.movies_checked_out_count = -1
+      result = customer.save
+      result.must_equal false
+
+      customer.movies_checked_out_count = 0
+      result = customer.save
+      result.must_equal true
+
+      customer.movies_checked_out_count = 1
+      result = customer.save
+      result.must_equal true
+    end
+  end
+
 end
