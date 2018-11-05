351aaa0fb611368fc1fb94ccbc130fc7d256f411
Jane <jane.edwards.mcnear@gmail.com>
2018-11-05 14:53:09 -0800

    added validations to rentals and customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -3,4 +3,6 @@ class Customer < ApplicationRecord
 
   validates :name, presence: true
 
+  validates :movies_checked_out_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
+
 end
--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,4 +1,12 @@
 class Rental < ApplicationRecord
+  STATUSES = ["checked out", "returned"]
+
   belongs_to :customer
   belongs_to :movie
+
+  validates :check_out_date, presence: true
+  validates :due_date, presence: true
+  validates :status, presence: true
+  validates :status,  presence: true, inclusion: { in: STATUSES }
+
 end
