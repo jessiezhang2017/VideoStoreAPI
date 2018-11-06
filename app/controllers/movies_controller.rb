ed80269f1d6b02b0093e0a6099d6866e141d963d
Jane <jane.edwards.mcnear@gmail.com>
2018-11-06 09:12:29 -0800

    made create pass smoke test

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -15,16 +15,16 @@ class MoviesController < ApplicationController
     movie = Movie.new(movie_params)
 
     if movie.save
-      render json: { ok: true, movie: movie.as_json( except: [:created_at, :updated_at] )}, status: :ok
+      render json: { id: movie.id }, status:  :ok
     else
       render json: { ok: false, errors: movie.errors.messages}, status: :bad_request
     end
   end
 
   private
-  
+
   def movie_params
-    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
+    params.permit(:title, :overview, :release_date, :inventory)
   end
 
 end
