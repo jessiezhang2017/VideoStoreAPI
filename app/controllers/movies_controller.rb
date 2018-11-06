c672b430493185fac1246674ae577c217d5b6cc8
Jane <jane.edwards.mcnear@gmail.com>
2018-11-05 17:04:44 -0800

    added movie create and tests

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,22 @@
 class MoviesController < ApplicationController
 
   def zomg
-      render json:{ ok: true, message: 'it works!'}, status: :ok
+    render json:{ ok: true, message: 'it works!'}, status: :ok
+  end
+
+  def create
+    movie = Movie.new(movie_params)
+
+    if movie.save
+      render json: { ok: true, movie: movie.as_json( except: [:created_at, :updated_at] )}, status: :ok
+    else
+      render json: { ok: false, errors: movie.errors.messages}, status: :bad_request
+    end
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :overview, :release_date, :inventory)
   end
 end
--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,7 +1,55 @@
 require "test_helper"
-
+require 'pry'
 describe MoviesController do
   # it "must be a real test" do
   #   flunk "Need real tests"
   # end
+
+  describe "create" do
+    let(:movie_data) {
+      {
+        title: "Jack",
+        overview: "American comedy-drama film starring Robin Williams and directed by Francis Ford Coppola.",
+        release_date: "1996-08-09",
+        inventory: 1
+      }
+    }
+
+    it "returns json" do
+      post create_movie_path, params: { movie: movie_data }
+      expect(response.header['Content-Type']).must_include 'json'
+    end
+
+    it "creates a movie with valid data and returns it's ID" do
+      expect {
+        post create_movie_path, params: { movie: movie_data }
+      }.must_change "Movie.count", 1
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body["movie"]).must_include "id"
+
+      movie = Movie.find(body["movie"]["id"].to_i)
+
+      expect(movie.title).must_equal movie_data[:title]
+      must_respond_with :success
+
+    end
+
+    it "fails to create a movie and returns an error with invalid data" do
+      movie_data["title"] = nil
+
+      expect {
+        post create_movie_path, params: { movie: movie_data }
+      }.wont_change "Movie.count"
+
+      body = JSON.parse(response.body)
+      expect(body).must_be_kind_of Hash
+      expect(body).must_include "errors"
+      expect(body["errors"]).must_include "title"
+      must_respond_with :bad_request
+    end
+
+  end
+
 end
