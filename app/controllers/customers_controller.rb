7ad8bb19b0af5871615937af6a78efbb89168927
Jane <jane.edwards.mcnear@gmail.com>
2018-11-05 16:21:04 -0800

    added customers API call and index method and tests for controller

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,7 @@
 class CustomersController < ApplicationController
+  def index
+    customers = Customer.all.order(:name)
+
+    render json: customers.as_json( only: [:id, :name, :registered_at, :postal_code, :phone, :movies_checked_out_count] ), status: :ok
+  end
 end
