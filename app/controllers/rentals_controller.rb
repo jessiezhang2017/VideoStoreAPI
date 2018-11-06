da10c2da852165c5cc3f663f9a5a6da3d65b1e23
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-11-06 13:18:33 -0800

    added the rantal checkout method

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,21 @@
 class RentalsController < ApplicationController
+
+  def check_out
+
+    rental = Rental.new
+
+    rental.customer = Customer.find_by(id: params[:customer_id])
+    rental.movie = Movie.find_by(id: params[:movie_id])
+    rental.check_out_date = Date.today
+
+    rental.due_date = rental.check_out_date + 7
+    rental.customer.movies_checked_out_count += 1
+    rental.movie.inventory -= 1
+
+    if rental.save
+      render json: { id: rental.id , checkout_date: rental.check_out_date, due_date:rental.due_date}, status:  :ok
+    else
+      render json: { ok: false, errors: rental.errors.messages}, status: :bad_request
+    end
+  end
 end
