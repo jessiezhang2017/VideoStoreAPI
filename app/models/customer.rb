a58808133ff758bc2180bed0193c60f3fab92103
Jessie Zhang <zhang_jessie2017@outlook.com>
2018-11-07 09:37:08 -0800

    move check sort method to model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -5,4 +5,13 @@ class Customer < ApplicationRecord
 
   validates :movies_checked_out_count, presence: true, numericality: { greater_than_or_equal_to: 0 }
 
+  def self.sort?(input)
+    valid_fields = ["name", "registered_at", "postal_code" ]
+    if valid_fields.include? (input)
+      return true
+    else
+      return false
+    end
+  end
+
 end
