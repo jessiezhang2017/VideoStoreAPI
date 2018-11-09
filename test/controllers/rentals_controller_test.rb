aac2316a5277dc6724a97a594a150342ca8d9f06
Jane <jane.edwards.mcnear@gmail.com>
2018-11-09 13:30:18 -0800

    completed rental controller tests

--- test/controllers/rentals_controller_test.rb
+++ test/controllers/rentals_controller_test.rb
@@ -74,58 +74,189 @@ describe RentalsController do
   end
 
   describe "check_in" do
-
-    it "finds the rental based off of the movie_id and customer_id params and 'checked out' status" do
-      # current_rental = Rental.find_by(movie_id: rental_params[:movie_id], customer_id: rental_params[:customer_id], status: "checked out")
-    end
+    let(:movie) { movies(:ring)  }
+    let(:customer) { customers(:lily)  }
+    let(:data) {
+      {
+        customer_id: customer.id,
+        movie_id: movie.id
+      }
+    }
 
     it "returns an error with invalid params/a rental doesn't exist" do
-      # if current_rental.nil?
-      #   render json: {ok: false, message: "Your rental was not found"}, status: :not_found
+      bad_data =       {
+        customer_id: "hello",
+        movie_id: "goodbye"
+      }
+      post check_in_movie_path, params: bad_data
+
+      body = JSON.parse(response.body)
+
+      expect(body["message"]).must_equal "Your rental was not found"
+      must_respond_with :not_found
     end
 
     it "succeeds with valid rental" do
-      # current_rental.check_in
-      # render json: {ok: true, message: "Movie successfully returned"}, status: :ok
+      post check_out_movie_path, params: data
+      post check_in_movie_path, params: data
+
+      body = JSON.parse(response.body)
+      expect(response.header['Content-Type']).must_include 'json'
+      expect(body["message"]).must_equal "Movie successfully returned"
+      must_respond_with :success
     end
+
     it "updates the rental's status and customer and movie inventories" do
-      # self.customer.check_in
-      # self.movie.check_in
-      # self.update(status: "returned")
+      post check_out_movie_path, params: data
+      checked_out_rental = Rental.last
+      id = checked_out_rental.id
+      movies_checked_out_count = checked_out_rental.customer.movies_checked_out_count
+      available_inventory = checked_out_rental.movie.available_inventory
+
+      post check_in_movie_path, params: data
+
+      checked_in_rental = Rental.find_by(id: id)
+
+      expect(checked_in_rental.status).must_equal "returned"
+      expect(checked_in_rental.customer.movies_checked_out_count).must_equal movies_checked_out_count - 1
+      expect(checked_in_rental.movie.available_inventory).must_equal available_inventory + 1
     end
   end
 
   describe "overdue" do
+    # get "/rentals/overdue", to: "rentals#overdue", as: "overdue"
+    # let(:check_out_data) {
+    #   {
+    #     sort: "toast"
+    #   }
+    # }
+
     it "renders json" do
+      get overdue_path
+
+      body = JSON.parse(response.body)
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
     end
 
     it "returns an error message if there are invalid sort params" do
-      # render json: {ok: false, message: overdue_rentals }, status: :not_found
+      bad_data =       {
+        sort: "hello"
+      }
+      get overdue_path, params: bad_data
+      must_respond_with :not_found
+
+      bad_data =       {
+        sort: "hello",
+        p: 1,
+        n: 1
+      }
+      get overdue_path, params: bad_data
+      must_respond_with :not_found
     end
 
     it "returns an error message if there are invalid p or n params" do
-
+      bad_data =       {
+        n: "hello",
+        p: "toast"
+      }
+      get overdue_path, params: bad_data
+      must_respond_with :not_found
     end
 
     it "returns a message if there are no overdue rentals" do
-      # elsif overdue_rentals.length == 0
-      #   render json: {ok: true, message: "There are no overdue rentals!"}, status: :ok
+      Rental.all.each do |rental|
+        rental.destroy if rental.due_date < Date.today
+      end
+
+      get overdue_path
+      must_respond_with :ok
+
+      body = JSON.parse(response.body)
+      expect(body["message"]).must_equal "There are no overdue rentals!"
     end
 
     it "returns overdue rentals with valid sort params" do
+      fields = %w(id check_out_date due_date status customer_id movie_id movie customer)
+      data =       {
+        sort: "name"
+      }
+      get overdue_path, params: data
+      get overdue_path
+      body = JSON.parse(response.body)
+
+      body.each do |rental|
+        expect(rental["status"]).must_equal "checked out"
+        rental.keys.must_equal fields
+        expect(rental["movie"]).must_include "title"
+        expect(rental["customer"]).must_include "name"
+        expect(rental["customer"]).must_include "postal_code"
+        expect(Date.parse(rental["due_date"])).must_be :<, Date.today
+      end
 
     end
 
     it "returns overdue rentals with valid p and n params" do
+      fields = %w(id check_out_date due_date status customer_id movie_id movie customer)
+      data =       {
+        n: "1",
+        p: "1"
+      }
 
+      get overdue_path, params: data
+
+      body = JSON.parse(response.body)
+      body.each do |rental|
+        expect(rental["status"]).must_equal "checked out"
+        rental.keys.must_equal fields
+        expect(rental["movie"]).must_include "title"
+        expect(rental["customer"]).must_include "name"
+        expect(rental["customer"]).must_include "postal_code"
+        expect(Date.parse(rental["due_date"])).must_be :<, Date.today
+      end
+
+      expect(body.length).must_equal data[:n].to_i
     end
 
     it "returns overdue rentals with valid p and n params and valid sort params" do
+      fields = %w(id check_out_date due_date status customer_id movie_id movie customer)
+      data =       {
+        n: "1",
+        p: "1",
+        sort: "name"
+      }
+
+      get overdue_path, params: data
+
+      body = JSON.parse(response.body)
+      body.each do |rental|
+        expect(rental["status"]).must_equal "checked out"
+        rental.keys.must_equal fields
+        expect(rental["movie"]).must_include "title"
+        expect(rental["customer"]).must_include "name"
+        expect(rental["customer"]).must_include "postal_code"
+        expect(Date.parse(rental["due_date"])).must_be :<, Date.today
+      end
+
+      expect(body.length).must_equal data[:n].to_i
 
     end
 
     it "returns overdue rentals with no params" do
-      # render :json => overdue_rentals, :include => {:movie => {:only => :title}, :customer => {:only => [:name, :postal_code]}}, :except => [:created_at, :updated_at], status: :ok
+      fields = %w(id check_out_date due_date status customer_id movie_id movie customer)
+
+      get overdue_path
+
+      body = JSON.parse(response.body)
+      body.each do |rental|
+        expect(rental["status"]).must_equal "checked out"
+        rental.keys.must_equal fields
+        expect(rental["movie"]).must_include "title"
+        expect(rental["customer"]).must_include "name"
+        expect(rental["customer"]).must_include "postal_code"
+        expect(Date.parse(rental["due_date"])).must_be :<, Date.today
+      end
     end
   end
+  
 end
